f792cf03df9e366fecff68fbd0ea38c8e5a56b80
Nguyen Cong Tuan Minh <126317770+tuanminhnc@users.noreply.github.com>
2023-06-08 12:46:19 +0000

    Baitap1b

--- Baitap1b.cpp
+++ Baitap1b.cpp
@@ -4,7 +4,7 @@
 //Begin main function
     int main() {
 /*
-(x^3-2x^2+x-1)*(5-x) = 5x^3 -10x^2 +5x -5 - x^4 -2x^3 -x^2 -x = -x +7x^3 -9x^2 +4x -5
+(x^3 -2x^2 +x-1)*(5 -x) = 5x^3 -10x^2 +5x -5 - x^4 -2x^3 -x^2 -x = -x +7x^3 -9x^2 +4x -5
 */
 //Declare variable x
     int x;
