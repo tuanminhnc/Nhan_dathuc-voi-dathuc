880191e0b8ed79f7a730ace358665b6ae63f8ce6
Nguyen Cong Tuan Minh <126317770+tuanminhnc@users.noreply.github.com>
2023-06-08 10:04:00 +0000

    Baitap1

---
+++ Baitap1.cpp
@@ -0,0 +1,16 @@
+//Declare libary
+#include <iostream>
+
+//Begin main function
+int main() {
+/*
+(x^2-2*x+1)*(x-1) = x2.x + x2.(– 1) + (– 2x).x + (–2x).(–1) + 1.x + 1.(–1) = x3 – 3x2 + 3x – 1     
+*/
+//Declare Variable x
+int x;
+//print to screen
+std::cout << "Nhap bien x: ";
+//input command
+    std::cin >> x;
+return 0;
+}
