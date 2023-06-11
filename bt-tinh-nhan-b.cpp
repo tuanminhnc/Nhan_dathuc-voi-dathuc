15a7152efe274abd7594c1a40af7f52583c6987b
Nguyen Cong Tuan Minh <126317770+tuanminhnc@users.noreply.github.com>
2023-06-11 12:48:25 +0000

    bai tap tinh nhan

---
+++ bt-tinh-nhan-b.cpp
@@ -0,0 +1,27 @@
+/*
+Bai tap tinh nhan b
+Time 7:31
+6/11/2023
+*/
+#include <iostream>
+using namespace std;
+
+//Begin main function
+int main() {
+
+/*
+1. Thong bao ra man hinh bai tap tinh nhan b
+2. Thuc hien tinh bieu thuc
+3. In ra ket qua
+*/
+
+//Buoc 1:
+cout << "Bai tap tinh nhan:" << endl;
+cout << "Bieu thuc: (x - 1/2y) * (x - 1/2y)" << endl;
+//Buoc 2:
+cout << "=> x * (x - 1/2y) - 1/2y * (x - 1/2y)" << endl;
+cout << "=> x^2 - 1/2xy - 1/2xy + 1/4y^2" << endl;
+//Buoc 3:
+cout << "=> x^2 -xy + 1/4y^2" << endl;
+    return 0;
+}
